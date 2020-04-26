edde6e918c85de90692d1754cf10e69c74411f4d
rodoviti <rodolfo.viti@everis.com>
2020-04-26 19:39:14 +0200

    minor fixes

--- api/rest/src/main/java/it/gov/pagopa/bpd/award_period/controller/BpdAwardPeriodControllerImpl.java
+++ api/rest/src/main/java/it/gov/pagopa/bpd/award_period/controller/BpdAwardPeriodControllerImpl.java
@@ -5,7 +5,6 @@ import it.gov.pagopa.bpd.award_period.assembler.AwardPeriodResourceAssembler;
 import it.gov.pagopa.bpd.award_period.model.entity.AwardPeriod;
 import it.gov.pagopa.bpd.award_period.model.resource.AwardPeriodResource;
 import it.gov.pagopa.bpd.award_period.service.AwardPeriodService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,6 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@Slf4j
 class BpdAwardPeriodControllerImpl extends StatelessController implements BpdAwardPeriodController {
 
     private final AwardPeriodService awardPeriodService;
@@ -33,16 +31,18 @@ class BpdAwardPeriodControllerImpl extends StatelessController implements BpdAwa
 
     @Override
     public AwardPeriodResource find(Long awardPeriodId) {
-        logger.debug("Start find by awardPeriodId");
-        logger.debug("awardPeriodId = [" + awardPeriodId + "]");
+        if (logger.isDebugEnabled()) {
+            logger.debug("BpdAwardPeriodControllerImpl.find");
+            logger.debug("awardPeriodId = [" + awardPeriodId + "]");
+        }
 
         try {
             final Optional<AwardPeriod> awardPeriod = awardPeriodService.find(awardPeriodId);
             return awardPeriodResourceAssembler.toResource(awardPeriod.get());
         } catch (
                 NoSuchElementException e) {
-            if (log.isErrorEnabled()) {
-                log.error(e.getMessage(), e);
+            if (logger.isErrorEnabled()) {
+                logger.error(e.getMessage(), e);
             }
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
@@ -51,7 +51,10 @@ class BpdAwardPeriodControllerImpl extends StatelessController implements BpdAwa
 
     @Override
     public List<AwardPeriodResource> findAll(OffsetDateTime offsetDateTime) {
-        logger.debug("Start findAll awardPeriod");
+        if (logger.isDebugEnabled()) {
+            logger.debug("BpdAwardPeriodControllerImpl.findAll");
+            logger.debug("offsetDateTime = [" + offsetDateTime + "]");
+        }
 
         LocalDate offsetDate = null;
         if (offsetDateTime != null) {
